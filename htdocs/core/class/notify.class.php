9e5e1b5312c06b708ebcedb853a86898cc4542ca
Ryad ABANI <91743099+RyAbn@users.noreply.github.com>
2025-10-22 00:45:15 +0200

    FIX: notification email not sent : NOTIFICATION_EMAIL_FROM is replaced with MAIN_MAIL_EMAIL_FROM if it's empty (#35881)
    Co-authored-by: Ryad ABANI <ryad.abani@scopen.fr>

--- htdocs/core/class/notify.class.php
+++ htdocs/core/class/notify.class.php
@@ -667,8 +667,7 @@ class Notify
 		if ($applicationcustom) {
 			$application = (preg_match('/^\+/', $applicationcustom) ? $application : '').$applicationcustom;
 		}
-
-		$from = getDolGlobalString('NOTIFICATION_EMAIL_FROM');
+		$from = getDolGlobalString('NOTIFICATION_EMAIL_FROM', getDolGlobalString('MAIN_MAIL_EMAIL_FROM'));
 		$object_type = '';
 		$link = '';
 		$num = 0;
